8bb9e54b205550011db80cce7bfc1b070a5122c3
Anatol-e <bolshoymedvedtoliano@gmail.com>
2020-09-10 19:01:12 +0300

    s

--- app/application.go
+++ app/application.go
@@ -14,7 +14,7 @@ func StartApplication() {
 
 	srv := &http.Server{
 		Handler: router,
-		Addr:    "127.0.0.1:8000",
+		Addr:    "127.0.0.1:8080",
 	}
 
 	if err := srv.ListenAndServe(); err != nil {
